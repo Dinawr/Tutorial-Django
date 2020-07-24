390f1215ad5ad2bdbbf2742ebcb95943e7f6569b
Dina Wahyu Rodita <66549384+Dinawr@users.noreply.github.com>
2020-07-24 21:56:42 +0700

    tutorial belajar django

---
+++ django-admin.py
@@ -0,0 +1,7 @@
+#!D:\Django\Project2\env\Scripts\python.exe
+# EASY-INSTALL-DEV-SCRIPT: 'Django==3.2.dev20200724090657','django-admin.py'
+__requires__ = 'Django==3.2.dev20200724090657'
+__import__('pkg_resources').require('Django==3.2.dev20200724090657')
+__file__ = 'D:\\Django\\Project2\\env\\Scripts\\django\\django\\bin\\django-admin.py'
+with open(__file__) as f:
+    exec(compile(f.read(), __file__, 'exec'))
